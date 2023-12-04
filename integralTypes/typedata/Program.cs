8b645ca2eebc038bc9f904e784ec2f75ffdd8e6f
Alfat Taufik Nurhidayat <145849640+AlfatTaufik@users.noreply.github.com>
2023-12-05 01:58:23 +0700

    Add files via upload

---
+++ integralTypes/typedata/Program.cs
@@ -0,0 +1,27 @@
+﻿// signal integral
+Console.WriteLine("Signed integral data types : ");
+
+Console.WriteLine($"sbyte   : {sbyte.MinValue} to {sbyte.MaxValue} ");
+Console.WriteLine($"short   : {short.MinValue} to {short.MaxValue}");
+Console.WriteLine($"int     : {int.MinValue} to {int.MinValue}");
+Console.WriteLine($"long    : {long.MinValue} to {long.MaxValue}");
+// Console.WriteLine($"")
+
+// unsigned integral
+Console.WriteLine("UnSigned integral data types : ");
+
+Console.WriteLine($"sbyte    : {sbyte.MinValue} to {sbyte.MaxValue} ");
+Console.WriteLine($"ushort   : {ushort.MinValue} to {ushort.MaxValue}");
+Console.WriteLine($"uint     : {uint.MinValue} to {uint.MaxValue}");
+Console.WriteLine($"ulong    : {ulong.MinValue} to {ulong.MaxValue}");
+// Console.WriteLine($"")
+
+Console.WriteLine("");
+Console.WriteLine("Floating point types:");
+Console.WriteLine($"float  : {float.MinValue} to {float.MaxValue} (with ~6-9 digits of precision)");
+Console.WriteLine($"double : {double.MinValue} to {double.MaxValue} (with ~15-17 digits of precision)");
+Console.WriteLine($"decimal: {decimal.MinValue} to {decimal.MaxValue} (with 28-29 digits of precision)");
+
+// reference value datatype
+int[] newData;
+newData = new int[3];
---
+++ parsingData/Program.cs
@@ -0,0 +1,10 @@
+﻿string value = "1642";
+int result = 0;
+if (int.TryParse(value, out result))
+{
+    Console.WriteLine($"Your result is good {80 + result}");
+}
+else
+{
+    Console.WriteLine("You have entered a bad number!");
+}
