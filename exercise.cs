c1aac4f064a066c7420bec7fc88ee2fc0ae8114b
Alfat Taufik Nurhidayat <145849640+AlfatTaufik@users.noreply.github.com>
2023-12-23 10:15:33 +0700

    Add files via upload

--- exercise.cs
+++ exercise.cs
@@ -1,17 +1,19 @@
-int value1 = 12;
-decimal value2 = 6.2m;
-float value3 = 4.3f;
+for (int outer = 0; outer < 5; outer++)
+{
+    for (int inner = 0; inner < 5; inner++)
+    {
+        if (inner > outer)
+        {
+            break;
+        }
 
-// Your code here to set result1
-int result1 = Convert.ToInt32((decimal)value1/value2);
-// Hint: You need to round the result to nearest integer (don't just truncate)
-Console.WriteLine($"Divide value1 by value2, display the result as an int: {result1}");
-
-// Your code here to set result2
-// decimal valueke3 = decimal.TryParse(value3, out valueke3);
-decimal result2 = value2 / (decimal)value3;
-Console.WriteLine($"Divide value2 by value3, display the result as a decimal: {result2}");
-
-// Your code here to set result3
-float result3 = value3 / (float)value1; 
-Console.WriteLine($"Divide value3 by value1, display the result as a float: {result3}");
+        Console.Write($"{inner} ");
+    }
+    Console.WriteLine();
+}
+// Output:
+// 0
+// 0 1
+// 0 1 2
+// 0 1 2 3
+// 0 1 2 3 4
